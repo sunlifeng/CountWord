4299560184bbb0b0ec8b7d8493d75acc24311f20
Sex LittleFish <s_lifeng@hotmail.com>
2013-12-05 20:08:32 +0800

    CountWord is for console countgui is for Gui

--- CountWord.py
+++ CountWord.py
@@ -6,10 +6,8 @@ import ConfigParser
 import argparse
 
 import pystardict
-import wx
 
-from countgui import FrameMain
-from countgui import SysOutListener
+
 
 def arguments():
     parser = argparse.ArgumentParser()
@@ -24,15 +22,103 @@ def arguments():
     return directory, filename
 
 
+def WhiteWord():
+    import re
+    f=open("wordlist.txt")
+    txt=f.read()
+    f.close()
+    d={}
+    s=[0]
+    def handle(m):
+            w=m.group(0)
+            w=w.lower()
+            d[w]=d.get(w,0)+1
+            s[0]+=1
+
+    txt=re.compile(r'\b[a-zA-Z]+\b').sub(handle,txt)
+    return d
+
+def WordCount(file):   
+    #file=raw_input("Enter source file:\n")
+    from time import clock
+    start=clock()
+    import re
+    f=open(file)
+    txt=f.read()
+    f.close()
+    d={}
+    s=[0]
+    def handle(m):
+            w=m.group(0)
+            w=w.lower()
+            d[w]=d.get(w,0)+1
+            s[0]+=1
+
+    txt=re.compile(r'\b[a-zA-Z]+\b').sub(handle,txt)
+    #print sorted(d.items(),key=lambda d:d[1])[-10:]
+
+    #print 'There are %d words in %s'%(s[0],file)
+    print 'There are %d words in %s'%(len(d),file)
+    finsih=clock()
+    #print 'Run time is %d s'%(finsih - start)
+    return d
+
+
+def longRunning(allfilename):
+        if allfilename==None:
+            print "Please open a text file first."
+            return;
+        f=open(allfilename+"_out.txt","w")
+        print "Output file will be %s"%(allfilename+"_out.txt")
+        d=WordCount(allfilename)
+        w=WhiteWord().keys()
+        print "Remove white word from list ..."
+        for wo in w:
+            d.pop(wo,None)
+        sort=sorted(d.items(),key=lambda d:d[1],reverse=True)
+        print "Word sorted and distincted\n"
+        config = ConfigParser.RawConfigParser()
+        config.read('dict.ini')
+        
+        dict_type = config.get('dict', 'type')
+        dict_data = config.get('dict', 'dict')
+        pydict=pystardict.Dictionary(dict_data+"\\"+dict_data)        
+        pylist=pydict.keys()         
+
+        print "Dictionary is %s"%(dict_data)  
+
+        print "Start checking dictionary...please wait\n"
+        #for (k,c) in d.items():
+        for i in sort:
+            f.write("\n\n"+i[0]+" "+str(i[1])+"\n")
+            searchword=i[0]
+            try:
+              f.write(str(pydict[searchword])) 
+            except Exception, e:
+                f.write(searchword+" not found in this dict.")
+                # try:
+                #     searchword="".join(difflib.get_close_matches(i[0],pylist)[0])
+                #     f.write("Maybe "+searchword+"?\n")
+                #     #"".join(searchword)                    
+                #     f.write(str(pydict[searchword]))     
+                # except Exception, e:
+                #     f.write("Really not found.\n")
+        print "Finished"
+        f.close()
+
+
+
 if __name__ == '__main__':
+    
+    file=raw_input("Enter source file:\n")
+    longRunning(file)
+    # app = wx.App(False)
+    # frame = FrameMain("CountWord")
+    # app.frame=frame
+    # directory, filename = arguments()
 
-    app = wx.App(False)
-    frame = FrameMain("CountWord")
-    app.frame=frame
-    directory, filename = arguments()
-
-    if filename != None:
-        frame.open(directory, filename)
-    sys.stdout = SysOutListener()
-    app.MainLoop()
+    # if filename != None:
+    #     frame.open(directory, filename)
+    # sys.stdout = SysOutListener()
+    # app.MainLoop()
  
--- countgui.py
+++ countgui.py
@@ -3,6 +3,7 @@
 
 import sys,os
 import ConfigParser
+import argparse
 import thread
 import difflib
 import wx
@@ -11,49 +12,9 @@ import wx.lib.mixins.listctrl as listmix
 import wx.grid as grid
 from wx.lib.newevent import NewEvent
 
-import pystardict
+from CountWord import WhiteWord,longRunning,WordCount
 
 
-def WordCount(file):   
-    #file=raw_input("Enter source file:\n")
-    from time import clock
-    start=clock()
-    import re
-    f=open(file)
-    txt=f.read()
-    f.close()
-    d={}
-    s=[0]
-    def handle(m):
-            w=m.group(0)
-            w=w.lower()
-            d[w]=d.get(w,0)+1
-            s[0]+=1
-
-    txt=re.compile(r'\b[a-zA-Z]+\b').sub(handle,txt)
-    #print sorted(d.items(),key=lambda d:d[1])[-10:]
-
-    #print 'There are %d words in %s'%(s[0],file)
-    print 'There are %d words in %s'%(len(d),file)
-    finsih=clock()
-    #print 'Run time is %d s'%(finsih - start)
-    return d
-
-def WhiteWord():
-    import re
-    f=open("wordlist.txt")
-    txt=f.read()
-    f.close()
-    d={}
-    s=[0]
-    def handle(m):
-            w=m.group(0)
-            w=w.lower()
-            d[w]=d.get(w,0)+1
-            s[0]+=1
-
-    txt=re.compile(r'\b[a-zA-Z]+\b').sub(handle,txt)
-    return d
 
 wxStdOut, EVT_STDDOUT= NewEvent()
 wxWorkerDone, EVT_WORKER_DONE= NewEvent()
@@ -192,50 +153,7 @@ class FrameMain(wx.Frame):
 
     def on_start(self,e):
         print "Start process...."
-        thread.start_new_thread(self.longRunning, ())
-
-    def longRunning(self):
-        if self.allfilename==None:
-            print "Please open a text file first."
-            return;
-        f=open(self.allfilename+"_out.txt","w")
-        print "Output file will be %s"%(self.allfilename+"_out.txt")
-        d=WordCount(self.allfilename)
-        w=WhiteWord().keys()
-        print "Remove white word from list ..."
-        for wo in w:
-            d.pop(wo,None)
-        sort=sorted(d.items(),key=lambda d:d[1],reverse=True)
-        print "Word sorted and distincted\n"
-        config = ConfigParser.RawConfigParser()
-        config.read('dict.ini')
-        
-        dict_type = config.get('dict', 'type')
-        dict_data = config.get('dict', 'dict')
-        pydict=pystardict.Dictionary(dict_data+"\\"+dict_data)        
-        pylist=pydict.keys()         
-
-        print "Dictionary is %s"%(dict_data)  
-
-        print "Start checking dictionary...please wait\n"
-        #for (k,c) in d.items():
-        for i in sort:
-            f.write("\n\n"+i[0]+" "+str(i[1])+"\n")
-            searchword=i[0]
-            try:
-              f.write(str(pydict[searchword])) 
-            except Exception, e:
-                f.write(searchword+" not found in this dict.")
-                # try:
-                #     searchword="".join(difflib.get_close_matches(i[0],pylist)[0])
-                #     f.write("Maybe "+searchword+"?\n")
-                #     #"".join(searchword)                    
-                #     f.write(str(pydict[searchword]))     
-                # except Exception, e:
-                #     f.write("Really not found.\n")
-        print "Finished"
-        f.close()
-
+        thread.start_new_thread(longRunning, (self.allfilename,))
     def OnUpdateOutputWindow(self, event):
         value = event.text
         self.logger.AppendText(value)
@@ -255,8 +173,6 @@ class FrameMain(wx.Frame):
         self.buttonOpen.SetToolTip(wx.ToolTip('Open File'))
         self.Bind(wx.EVT_BUTTON, self.on_start, self.buttonStart)
         self.Bind(wx.EVT_BUTTON, self.OnOpen, self.buttonOpen)
-
-
 
         grid = wx.GridBagSizer(5, 5)
 
@@ -296,3 +212,28 @@ class FrameMain(wx.Frame):
 EVT_THREAD_STATUS = wx.NewId()
 def thread_event_handler(win, event, function):
     win.Connect(-1, -1, event, function)
+
+
+def arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="plot filename", nargs='?')
+    args = parser.parse_args()
+
+    filename = None
+    directory = None
+    if args.file != None:
+        directory, filename = os.path.split(args.file)
+
+    return directory, filename
+
+if __name__ == '__main__':
+
+    app = wx.App(False)
+    frame = FrameMain("CountWord")
+    app.frame=frame
+    directory, filename = arguments()
+
+    if filename != None:
+        frame.open(directory, filename)
+    sys.stdout = SysOutListener()
+    app.MainLoop()
